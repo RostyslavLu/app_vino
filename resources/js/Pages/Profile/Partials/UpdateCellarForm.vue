4d3c181f9dd6055796d1ad0acd6a73d75c9f1fac
nevge336 <neveu.ge@gmail.com>
2024-02-22 16:02:33 -0500

    mettre à jour mes celliers est enlever description

--- resources/js/Pages/Profile/Partials/UpdateCellarForm.vue
+++ resources/js/Pages/Profile/Partials/UpdateCellarForm.vue
@@ -38,17 +38,19 @@ const save = () => {
         <div>
             <InputError :message="form.errors.name" />
             <div v-if="!editingName" class="flex-row">
+                <img src="/img/cellar-blanc.png" class="icon">
+
                 <span>{{ form.name }}</span>
                 <img src="/img/icons/edit-3.svg" alt="Edit" @click="startEditingName">
             </div>
             <div v-else>
                 <div class="flex-row">
                     <TextInput
-                    id="name"
+                    id="cellar_name"
                     type="text"
                     v-model="form.name"
                     autofocus
-                    autocomplete="name"
+                    autocomplete="nom du cellier"
                     />
                     <img src="/img/icons/check.svg" alt="Accept" @click="save">
                     <img src="/img/icons/x.svg" alt="Cancel" @click="stopEditing">
