15ca89b701e2e4ef49265bb4fbbff6f993c658c3
melisandreschofield <melisandre.schofield@gmail.com>
2024-02-23 10:43:35 -0500

    début de page SaqWines. à integrer dans ce qui existe déjà.

--- resources/js/Pages/SaqWines.vue
+++ resources/js/Pages/SaqWines.vue
@@ -1,24 +1,23 @@
 <script setup>
 import { Link, usePage } from '@inertiajs/vue3';
-
-const { wines } = usePage().props.wines;
+const  wines = usePage().props.wines;
+console.log(wines);
 </script>
-
-
 <template>
-  <div>
-    <h1>Les vins</h1>
-    <ul>
-
-      <li v-for="wine in $wines" :key="wine.id">
-        {{ wine.name }}
-      </li>
-    </ul>
-    
-    <!-- Pagination -->
-<!--     <div>
-      <Link v-if="wines.prev_page_url" :href="wines.prev_page_url">Previous</Link>
-      <Link v-if="wines.next_page_url" :href="wines.next_page_url">Next</Link>
-    </div> -->
-  </div>
+    <div>
+        <h1>Les vins</h1>
+        <ul>
+            <li v-for="wine in wines.data">
+                {{ wine.name }}
+            </li>
+        </ul>
+        <!-- Pagination -->
+        <div>
+            <Link v-if="wines.prev_page_url" :href="wines.prev_page_url">Previous</Link>
+            <Link v-if="wines.next_page_url" :href="wines.next_page_url">Next</Link>
+            <!-- nombre de pages -->
+            total pages: {{ wines.last_page }}
+        </div>
+        
+    </div>
 </template>
