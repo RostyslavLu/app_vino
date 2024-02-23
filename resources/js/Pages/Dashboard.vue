088dd4f1b9dea760a747f0e456f2201c0a3e2629
melisandreschofield <melisandre.schofield@gmail.com>
2024-02-23 13:06:45 -0500

    montrer les vins de la saq

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -53,7 +53,7 @@ fetchUserCellars();
             <div class="user-cellars">
                 <select-input v-model="selectedCellar" :options="userCellars"
                     @change="fetchUserCellarContent($event); clearSearch()"></select-input>
-                <Link :href="route('addWineToCellar')">
+                <Link :href="route('add-wine-cellar')">
                 <svg style="display: inline-block; vertical-align: middle; cursor: pointer;" width="24" aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
