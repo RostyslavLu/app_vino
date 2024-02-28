7582cf63734f308cf40be60eb7cf396d86980782
melisandreschofield <melisandre.schofield@gmail.com>
2024-02-28 09:00:17 -0500

    affichage WineListItem vs WineDetail avec toggle, sans css

---
+++ resources/js/Components/ToggleWineDetail.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    showDetail: {
+        type: Boolean,
+        required: true
+    }
+});
+
+const toggleDetail = () => {
+    showDetail.value = !showDetail.value;
+}
+
+</script>
+<template>
+<div class="flex-row">
+    <button @click="toggleDetail" v-if="!showDetail">
+        <!-- plus square -->
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-square"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg>
+    </button>
+    <button @click="toggleDetail" v-if="showDetail">
+        <!-- minus square -->
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-minus-square"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg>
+    </button>
+    <div class="line"></div>
+</div>
+</template>
---
+++ resources/js/Components/WineDetail.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { defineProps } from 'vue';
+import AddSaqWineButton from '@/Components/AddSaqWineButton.vue';
+
+const props = defineProps({
+    content: {
+        type: Object,
+        required: true
+    }
+});
+
+// fonction pour marquer la couleur du type de vin
+const getBackgroundColor = (wineColor) => {
+    switch (wineColor) {
+        case 'rouge':
+            return 'var(--wine-red)';
+        case 'blanc':
+            return 'var(--wine-white)';
+        case 'rosé':
+            return 'var(--wine-rose)';
+        default:
+            return 'transparent';
+    }
+};
+</script>
+
+<template>
+
+    <picture class="wine-image">
+        <div class="wine-type" :style="{ backgroundColor: getBackgroundColor(content.type) }"></div>
+        <source :srcset="content.url_image" type="image/webp" />
+        <img :src="content.url_image" :alt="content.name" />
+    </picture>
+    <div class="content-info">
+        <h3>{{ content.name }}</h3>
+        <div class="wine-details">
+            <div class="wine-info">
+                <p>{{ content.description }}</p>
+            </div>
+            <div class="wine-quantity">
+                <AddSaqWineButton :id="content.id"/>
+            </div>
+
+        </div>
+    </div>
+</template>
