3e7bdbf9fe6a23da18b413b06e4a9bc9cb290259
melisandreschofield <melisandre.schofield@gmail.com>
2024-02-28 15:27:44 -0500

    recherche dans le cellier. requete sql. reste a repatrier les données pour qu'ils matchent le système du dashboard

--- app/Http/Controllers/CellarSearchController.php
+++ app/Http/Controllers/CellarSearchController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use App\Models\Cellar_content;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Cellars;
+use App\Models\User;
 use Inertia\Inertia;
 
 use Illuminate\Http\Request;
@@ -14,30 +15,26 @@ class CellarSearchController extends Controller
     //rechercher dans les vins de la SAQ
     public function search($search){
         //récupérer l'id de l'utilisateur
-        $user = Auth::id();
+        $user = Auth::user();
+
         //récupérer les celliers de l'utilisateur
-        $userCellar = Cellars::where('user_id', $user)->get();
-        //récupérer les vins dans le cellier de l'utilisateur
-   /*      $cellarContents = Cellar_content::where('cellars_id', $userCellar[0]->id)->get(); */
+        $userCellar = Cellars::where('user_id', $user->id)->get();
 
-        $cellarContents = Cellar_content::join('types', $userCellar[0]->id)->get();
+        //récupérer le premier cellier de l'utilisateur
+        $firstCellarId = $user->cellars()->orderBy('created_at')->first()->id;
 
-/*         $wines = Cellar_content::join('types', 'saq_wines.types_id', '=', 'types.id') 
-            ->select('saq_wines.*', 'types.type')
-            ->where('name', 'like', "%$search%")
-            ->paginate(20);
+        $cellarContents = Cellar_content::where('cellars_id', $firstCellarId)
+            ->join('saq_wines', 'cellar_contents.saq_wines_id', '=', 'saq_wines.id')
+            ->join('types', 'saq_wines.types_id', '=', 'types.id')
+            ->select('saq_wines.*', 'types.type', 'cellar_contents.quantity')
+            ->where('name', 'like', "%$search%");
 
-        return Inertia::render('AddWineToCellar', [
-            'wines' => $wines, 
-            'search' => $search,
-        ]);
-
+        dd($cellarContents);
          //retourner la vue
-         return Inertia::render('Dashboard', [
+/*          return Inertia::render('Dashboard', [
             'userCellar' => $userCellar,
             'cellarContents' => $cellarContents,
             'wines' => $wines,
-        ]);*/
+        ]); */
     }
-
 }
