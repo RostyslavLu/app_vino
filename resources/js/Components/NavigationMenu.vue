22ae99f0fce14c778ad57595b358d4354455740c
nevge336 <neveu.ge@gmail.com>
2024-02-29 12:35:05 -0500

    design css du desktop-menu pour format adaptatif

--- resources/js/Components/NavigationMenu.vue
+++ resources/js/Components/NavigationMenu.vue
@@ -62,9 +62,23 @@ const toggleMenu = () => {
             </div>
         </div>
         <div class="desktop-menu">
-            <Link href="/profile">Profile</Link>
+            <div>   
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user icon"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
+                <Link href="/profile">Profile</Link>
+
+            </div>
             <Link href="/dashboard">Cellar</Link>
-            <Link href="/logout">Logout</Link>
+            <div class="flex-column">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-out icon"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line x1="21" y1="12" x2="9" y2="12"></line></svg>
+                <Link 
+                    href="/logout"
+                    class="__logout" 
+                    method="post" 
+                    as="button" 
+                    type="button"
+                    >Logout</Link
+                >
+            </div>
         </div>
     </div>
 </template>
