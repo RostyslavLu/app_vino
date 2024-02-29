c4aeb5b42eaa8cc1d101e5faa32607cb3ce57fb4
nevge336 <neveu.ge@gmail.com>
2024-02-29 12:07:46 -0500

    faire apparaître et disparaître le menu selon la largeur de la page

--- resources/js/Components/NavigationMenu.vue
+++ resources/js/Components/NavigationMenu.vue
@@ -15,7 +15,7 @@ const toggleMenu = () => {
 
 <template>
     <div>
-        <img v-if="!showMenu" src="/img/icons/menu.svg" alt="menu" class="icon" @click="toggleMenu"/>
+        <img v-if="!showMenu" src="/img/icons/menu.svg" alt="menu" class="icon hamburger-icon" @click="toggleMenu"/>
         <svg v-else xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x icon-x-menu" @click="toggleMenu">
             <line x1="18" y1="6" x2="6" y2="18"></line>
             <line x1="6" y1="6" x2="18" y2="18"></line>
@@ -61,5 +61,29 @@ const toggleMenu = () => {
                 >
             </div>
         </div>
+        <div class="desktop-menu">
+            <Link href="/profile">Profile</Link>
+            <Link href="/dashboard">Cellar</Link>
+            <Link href="/logout">Logout</Link>
+        </div>
     </div>
 </template>
+<style scoped>
+.hamburger-icon, .desktop-menu {
+    display: flex;
+}
+
+.desktop-menu {
+    display: none;
+}
+
+@media (min-width: 600px) {
+    .hamburger-icon {
+        display: none;
+    }
+
+    .desktop-menu {
+        display: flex;
+    }
+}
+</style>
